e0cfec1cc017df9a469e94ac7523b158d1fa965d
HozayAndrew <dj15.exe@gmail.com>
2019-06-12 23:33:50 +0300

    Update Config.cs

--- TeacherScheduleProblem/TeacherProblemApp/Config.cs
+++ TeacherScheduleProblem/TeacherProblemApp/Config.cs
@@ -8,14 +8,14 @@ namespace TeacherProblemApp
 {
     public class Config
     {
-        public static string GreedyAlgorithm = "https://teacherproblem.azurewebsites.net/api/algorithms/greedy";
-        public static string AntColonyAlgorithm = "https://teacherproblem.azurewebsites.net/api/algorithms/antColony";
-        public static string BeesAlgorithm = "http://busovilya.pythonanywhere.com/api/algorithms/bees";
+        public static string GreedyAlgorithm = "";
+        public static string AntColonyAlgorithm = "";
+        public static string BeesAlgorithm = "";
 
-        public static string GreedyAlgorithmExperiments = "https://teacherproblem.azurewebsites.net/api/experiments/greedy";
-        public static string AntColonyAlgorithmExperiments = "https://teacherproblem.azurewebsites.net/api/experiments/antColony";
-        public static string BeesAlgorithmExperiments = "http://busovilya.pythonanywhere.com/api/experiments/bees";
+        public static string GreedyAlgorithmExperiments = "";
+        public static string AntColonyAlgorithmExperiments = "";
+        public static string BeesAlgorithmExperiments = "";
 
-        public static string GenerateRequest = "https://teacherproblem.azurewebsites.net/api/experiments";
+        public static string GenerateRequest = "";
     }
 }
